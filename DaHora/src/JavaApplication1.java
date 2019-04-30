90990039a8c66f9bef551710f9ed4ce904491255
FelMelo <felipecmelo95@icloud.com>
2019-04-30 13:39:16 -0300

    ler escrever arquivar

--- DaHora/src/JavaApplication1.java
+++ DaHora/src/JavaApplication1.java
@@ -1,17 +1,18 @@
 import java.io.*;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class JavaApplication1 {
+
+
     
    public static void main (String arg []){
-	  
-         // menu principal
+	   
+	   
+	//  menu principal
 		int opcao = 0;
 		Scanner lerOption = new Scanner(System.in);
 		do {
-                        System.out.println("              \n\n#### DaHora - Banco de Horas  ####");
+			System.out.println("\n\n### DaHora - Banco de Horas ###");
 			System.out.println("\n                  ================================");
 			System.out.println("                  |     1 - Cadastro Funcionário   |");
 			System.out.println("                  |     2 - Cadastro Empresa       |");
@@ -22,7 +23,8 @@ public class JavaApplication1 {
 			System.out.println("                  |     0 - Sair                   |");
 			System.out.println("                  =================================\n");
 			System.out.println                    ("Opção -> ");
-			opcao = lerOption.nextInt();			
+			opcao = lerOption.nextInt();
+			
 			System.out.print("\n");
 			switch (opcao) {
 			case 1: CadastroFuncionario();
@@ -49,30 +51,43 @@ public class JavaApplication1 {
 				break;
 			}
 		} while (opcao != 0);
-		System.out.println("Programa encerrado.");		
+		
+		System.out.println("Programa encerrado.");
+		
    }
-   private static void CadastroFuncionario() {       
+
+   private static void CadastroFuncionario() {
+       
        Scanner ler = new Scanner(System.in);
-            // Conteudo
-            System.out.printf("Informe seu nome:\n");
-            String nome = ler.nextLine();
-            System.out.printf("Informe seu CPF:\n");
-            String cpf = ler.nextLine();				
-            try {
+       // Conteudo
+
+		System.out.printf("Informe seu nome:\n");
+		String nome = ler.nextLine();
+		System.out.printf("Informe seu CPF:\n");
+		String cpf = ler.nextLine();
+		
+		   
+		
+			try {
+
            // Cria arquivo
            File file = new File("teste.txt");
+
            // Se o arquivo nao existir, ele gera
            if (!file.exists()) {
                file.createNewFile();
            }
+
            // Prepara para escrever no arquivo
            FileWriter fw = new FileWriter(file.getAbsoluteFile());
            // Escreve e fecha arquivo
            try (BufferedWriter bw = new BufferedWriter(fw)) {
                // Escreve e fecha arquivo
                bw.write(nome + "\n");
-               bw.write(cpf + "\n");               
-           }           
+               bw.write(cpf + "\n");
+               
+           }
+           
            // Le o arquivo
            FileReader ler1 = new FileReader("teste.txt");
            BufferedReader reader = new BufferedReader(ler1);  
@@ -80,79 +95,35 @@ public class JavaApplication1 {
            while( (linha = reader.readLine()) != null ){
                System.out.println(linha);
            }
+
            // Imprime confirmacao
            System.out.println("Feito =D");
+
        } catch (IOException e) {
-       }       
+       }
+       
    }
+
    private static void CadastroEmpresa() {
        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
    }
+
    private static void ConsultaFuncionario() {
        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
    }
+
    private static void ConsultaEmpresa() {
        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
    }
+
    private static void FazerCheckIn() {
        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
    }
+
    private static void FazerCheckOut() {
        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-   }                     
+   }
+	   
 
-	Scanner ler = new Scanner(System.in);
-        // Conteudo
-        System.out.printf("Informe Seu nome:\n");
-	String content = ler.nextLine();
-        try {
-        // Cria arquivo
-        File file = new File("teste.txt");
-        // Se o arquivo nao existir, ele gera
-        if (!file.exists()) {
-             try {
-                file.createNewFile();
-           } catch (IOException ex) {
-                    Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
-           }
-           }
-        // Prepara para escrever no arquivo
-            FileWriter fw = null;
-        try {
-            fw = new FileWriter(file.getAbsoluteFile());
-        } catch (IOException ex) {
-            Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        // Escreve e fecha arquivo
-            try (BufferedWriter bw = new BufferedWriter(fw)) {
-                try {
-        // Escreve e fecha arquivo
-                    bw.write(content);
-                } catch (IOException ex) {
-                    Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            } catch (IOException ex) {
-           Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
-        }            
-        // Le o arquivo
-            FileReader ler1 = null;
-        try {
-            ler1 = new FileReader("teste.txt");
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
-        }
-            BufferedReader reader = new BufferedReader(ler1);  
-            String linha;
-       try {
-           while( (linha = reader.readLine()) != null ){
-               System.out.println(linha);
-           }
-        } catch (IOException ex) {
-           Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        // Imprime confirmacao
-            System.out.println("Feito =D");
-        } catch (IOException e) {
-        }
-}  
-
+                     
+}
