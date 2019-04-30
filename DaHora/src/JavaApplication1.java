090a9f8287f7afb0b78103faf7a1759908989ef8
FelMelo <felipecmelo95@icloud.com>
2019-04-30 13:43:25 -0300

    cadastro empresa

--- DaHora/src/JavaApplication1.java
+++ DaHora/src/JavaApplication1.java
@@ -105,7 +105,51 @@ public class JavaApplication1 {
    }
 
    private static void CadastroEmpresa() {
-       throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	   
+	   Scanner ler = new Scanner(System.in);
+       // Conteudo
+
+		System.out.printf("Informe o nome da empresa:\n");
+		String nomeEmp = ler.nextLine();
+		System.out.printf("Informe CNPJ:\n");
+		String cnpj = ler.nextLine();
+		
+		   
+		
+			try {
+
+           // Cria arquivo
+           File file = new File("teste.txt");
+
+           // Se o arquivo nao existir, ele gera
+           if (!file.exists()) {
+               file.createNewFile();
+           }
+
+           // Prepara para escrever no arquivo
+           FileWriter fw = new FileWriter(file.getAbsoluteFile());
+           // Escreve e fecha arquivo
+           try (BufferedWriter bw = new BufferedWriter(fw)) {
+               // Escreve e fecha arquivo
+               bw.write(nomeEmp + "\n");
+               bw.write(cnpj + "\n");
+               
+           }
+           
+           // Le o arquivo
+           FileReader ler1 = new FileReader("teste.txt");
+           BufferedReader reader = new BufferedReader(ler1);  
+           String linha;
+           while( (linha = reader.readLine()) != null ){
+               System.out.println(linha);
+           }
+
+           // Imprime confirmacao
+           System.out.println("Feito =D");
+
+       } catch (IOException e) {
+       }
+	   
    }
 
    private static void ConsultaFuncionario() {
