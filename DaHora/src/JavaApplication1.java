ee323a23c174e62d06dad2276aec634a40579ec9
Rajha Fajardo <rajhafajardo@hotmail.com>
2019-04-30 13:59:30 -0300

    formatação

--- DaHora/src/JavaApplication1.java
+++ DaHora/src/JavaApplication1.java
@@ -2,17 +2,12 @@ import java.io.*;
 import java.util.Scanner;
 
 public class JavaApplication1 {
-
-
-    
    public static void main (String arg []){
-	   
-	   
 	//  menu principal
 		int opcao = 0;
 		Scanner lerOption = new Scanner(System.in);
 		do {
-			System.out.println("\n\n### DaHora - Banco de Horas ###");
+			System.out.println("\n\n              ##### DaHora - Banco de Horas #####");
 			System.out.println("\n                  ================================");
 			System.out.println("                  |     1 - Cadastro Funcionário   |");
 			System.out.println("                  |     2 - Cadastro Empresa       |");
@@ -52,42 +47,30 @@ public class JavaApplication1 {
 			}
 		} while (opcao != 0);
 		
-		System.out.println("Programa encerrado.");
-		
+		System.out.println("Programa encerrado.");		
    }
-
-   private static void CadastroFuncionario() {
-       
+   private static void CadastroFuncionario() {       
        Scanner ler = new Scanner(System.in);
        // Conteudo
-
 		System.out.printf("Informe seu nome:\n");
 		String nome = ler.nextLine();
 		System.out.printf("Informe seu CPF:\n");
 		String cpf = ler.nextLine();
-		
-		   
-		
 			try {
-
            // Cria arquivo
            File file = new File("teste.txt");
-
            // Se o arquivo nao existir, ele gera
            if (!file.exists()) {
                file.createNewFile();
            }
-
            // Prepara para escrever no arquivo
            FileWriter fw = new FileWriter(file.getAbsoluteFile());
            // Escreve e fecha arquivo
            try (BufferedWriter bw = new BufferedWriter(fw)) {
                // Escreve e fecha arquivo
                bw.write(nome + "\n");
-               bw.write(cpf + "\n");
-               
-           }
-           
+               bw.write(cpf + "\n");               
+           }           
            // Le o arquivo
            FileReader ler1 = new FileReader("teste.txt");
            BufferedReader reader = new BufferedReader(ler1);  
@@ -95,47 +78,33 @@ public class JavaApplication1 {
            while( (linha = reader.readLine()) != null ){
                System.out.println(linha);
            }
-
            // Imprime confirmacao
            System.out.println("Feito =D");
-
        } catch (IOException e) {
-       }
-       
+       }       
    }
-
-   private static void CadastroEmpresa() {
-	   
+   private static void CadastroEmpresa() {	   
 	   Scanner ler = new Scanner(System.in);
        // Conteudo
-
 		System.out.printf("Informe o nome da empresa:\n");
 		String nomeEmp = ler.nextLine();
 		System.out.printf("Informe CNPJ:\n");
-		String cnpj = ler.nextLine();
-		
-		   
-		
+		String cnpj = ler.nextLine();  		
 			try {
-
            // Cria arquivo
            File file = new File("teste.txt");
-
            // Se o arquivo nao existir, ele gera
            if (!file.exists()) {
                file.createNewFile();
            }
-
            // Prepara para escrever no arquivo
            FileWriter fw = new FileWriter(file.getAbsoluteFile());
            // Escreve e fecha arquivo
            try (BufferedWriter bw = new BufferedWriter(fw)) {
                // Escreve e fecha arquivo
                bw.write(nomeEmp + "\n");
-               bw.write(cnpj + "\n");
-               
-           }
-           
+               bw.write(cnpj + "\n");               
+           }           
            // Le o arquivo
            FileReader ler1 = new FileReader("teste.txt");
            BufferedReader reader = new BufferedReader(ler1);  
@@ -143,31 +112,21 @@ public class JavaApplication1 {
            while( (linha = reader.readLine()) != null ){
                System.out.println(linha);
            }
-
            // Imprime confirmacao
            System.out.println("Feito =D");
-
        } catch (IOException e) {
-       }
-	   
+       }	   
    }
-
    private static void ConsultaFuncionario() {
        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
    }
-
    private static void ConsultaEmpresa() {
        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
    }
-
    private static void FazerCheckIn() {
        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
    }
-
    private static void FazerCheckOut() {
        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-   }
-	   
-
-                     
+   }                       
 }
